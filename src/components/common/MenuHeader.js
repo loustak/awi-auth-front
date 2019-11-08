74897305c221bc25048048c472482d6bffb888c3
Alia Chawaf <alia.chawaf@etu.umontpellier.fr>
2019-11-08 01:33:40 +0100

    ADD - MenuHeader Component

---
+++ src/components/common/MenuHeader.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import logo from '../../assets/logoMyDash.svg'
+import { Button, Container, Dropdown, Icon, Image, Menu } from 'semantic-ui-react'
+
+class MenuHeader extends React.Component {
+  state = { activeItem: 'dashboard' }
+
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
+  render () {
+    const { activeItem } = this.state
+
+    return (
+      <div>
+        <Menu fixed='top' stackable pointing>
+          <Container>
+            <Menu.Item as='a' disabled>
+              <Image size='tiny' src={logo} />
+            </Menu.Item>
+
+            <Menu.Item
+              as='a'
+              name='dashboard'
+              active={activeItem === 'dashboard'}
+              onClick={this.handleItemClick}
+            />
+
+            <Dropdown item simple text='Statistics'>
+              <Dropdown.Menu>
+                <Dropdown.Item>General</Dropdown.Item>
+                <Dropdown.Item>Courses</Dropdown.Item>
+                <Dropdown.Item>...</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+
+            <Menu.Item as='a' position='right'>
+              <Button> <Icon name='log out' color='red' /> Log out </Button>
+            </Menu.Item>
+          </Container>
+        </Menu>
+      </div>
+
+    )
+  }
+}
+
+export default MenuHeader
