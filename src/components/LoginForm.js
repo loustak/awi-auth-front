6b0aaadf2d926d953b439d8547ea8714b938277a
Alia Chawaf <alia.chawaf@etu.umontpellier.fr>
2019-11-09 12:15:22 +0100

    STYLE - Login

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,24 +1,49 @@
 import React from 'react'
-import { Button, Form, Segment } from 'semantic-ui-react'
+import { Button, Form } from 'semantic-ui-react'
 
 class LoginForm extends React.Component {
+
+  constructor (props) {
+    super(props)
+    this.state = {
+      email: '',
+      password: ''
+    }
+  }
+
+  handleEmailChange (event) {
+    this.setState({ email: event.target.value })
+  }
+
+  handlePasswordChange (event) {
+    this.setState({ password: event.target.value })
+  }
+
   render () {
+    const isEnabled = this.state.email.length > 0 && this.state.password.length > 0
+
     return (
       <Form size='large'>
-        <Segment>
-          <Form.Input fluid icon='user' iconPosition='left' placeholder='E-mail address' />
-          <Form.Input
-            fluid
-            icon='lock'
-            iconPosition='left'
-            placeholder='Password'
-            type='password'
-          />
+        <Form.Input
+          fluid
+          icon='user'
+          iconPosition='left'
+          placeholder='E-mail address'
+          onChange={this.handleEmailChange.bind(this)}
+        />
 
-          <Button color='teal' fluid size='large'>
-            Login
-          </Button>
-        </Segment>
+        <Form.Input
+          fluid
+          icon='lock'
+          iconPosition='left'
+          placeholder='Password'
+          type='password'
+          onChange={this.handlePasswordChange.bind(this)}
+        />
+
+        <Button color='teal' fluid size='large' disabled={!isEnabled}>
+          Login
+        </Button>
       </Form>
     )
   }
--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import '../../styles/App.css'
 import LoginForm from '../LoginForm.js'
-import { Grid, Image } from 'semantic-ui-react'
+import { Grid, Image, Divider } from 'semantic-ui-react'
 import logo from '../../assets/logoMyDash.svg'
 
 const Login = () => {
   return (
     <Grid centered textAlign='center' style={{ height: '50vh' }} verticalAlign='middle'>
-      <Grid.Column style={{ maxWidth: 450 }}>
+      <Grid.Column style={{ maxWidth: 300 }}>
         <Image src={logo} size='medium' centered />
+        <Divider hidden />
         <LoginForm />
       </Grid.Column>
     </Grid>
