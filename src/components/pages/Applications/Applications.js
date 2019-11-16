171f6159dd029396633707fac6062f03bcec2735
Samson Audrey <audrey.samson29@gmail.com>
2019-11-16 10:42:27 +0100

    add All Apps

--- src/components/pages/Applications/Applications.js
+++ src/components/pages/Applications/Applications.js
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Collapse from "../../Collapse/Collapse";
 import ApplicationItem from "../../CollapseItems/ApplicationItem/ApplicationItem";
 import EmptyItem from "../../CollapseItems/EmptyItem/EmptyItem";
+import Form from 'react-bootstrap/Form';
+
+
 
 const apps = [
     { name: 'App1', url: 'url' },
@@ -9,7 +12,10 @@ const apps = [
     { name: 'App3', url: 'url' },
 ]
 
+
 class Applications extends React.Component {
+
+
     render () {
         return (
             <div className='applicationItem'>
@@ -23,7 +29,40 @@ class Applications extends React.Component {
                     }
                 </Collapse>
                 <br/>
-
+                <Collapse title='Toutes les applications'>
+                    <div>
+                        {
+                            apps.length > 0
+                                ? <div>
+                                    <Form.Control
+                                        type="text"
+                                        placeholder="Rechercher"
+                                        //onChange={e => setSearch(e.target.value.toLowerCase())}
+                                    />
+                                </div>
+                                : null
+                        }
+                        {
+                            // eslint-disable-next-line no-undef
+                            apps.filter(app => app.name.toLowerCase().includes("search")).map((app, i) => // TODO
+                                <div key={i}>
+                                    <div />
+                                    <div>
+                                        <div >{app.name}</div>
+                                        <div >{app.url}</div>
+                                    </div>
+                                </div>,
+                            )
+                        }
+                    </div>
+                    {
+                        apps.length > 0
+                            ? apps.map((app, i) =>
+                                <ApplicationItem name={app.name} status={app.url} key={i}/>,
+                            )
+                            : <EmptyItem/>
+                    }
+                </Collapse>
             </div>
         )
     }
