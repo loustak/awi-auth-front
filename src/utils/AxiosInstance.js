e37bcad0869fb77beb91fd4d0a6721e1838ac8e9
Alia Chawaf <alia.chawaf@etu.umontpellier.fr>
2019-11-18 14:59:32 +0100

    REMOVE - Env var SERVER_URL

--- src/utils/AxiosInstance.js
+++ src/utils/AxiosInstance.js
@@ -1,8 +1,7 @@
 import axios from 'axios'
-import * as process from 'prop-types'
 
 const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_SERVER_URL,
+  baseURL: 'http://mydash-dev.igpolytech.fr',
   timeout: 2000,
   responseType: 'json'
 })
