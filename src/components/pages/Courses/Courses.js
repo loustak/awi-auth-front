dab43827d04f44854444b7ddc3d00e0e33c1ead2
Samson Audrey <audrey.samson29@gmail.com>
2019-11-18 15:59:41 +0100

    Add 'intervenants' to student dashboard

--- src/components/pages/Courses/Courses.js
+++ src/components/pages/Courses/Courses.js
@@ -7,7 +7,7 @@ import { Col } from 'react-bootstrap'
 
 const courses = [
   { name: 'Cours 1', teacher:'Arnaud Castelltort', hours:'33', coef:'1', description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' },
-  { name: 'Cours 2', teacher:'Arnaud Castelltort', hours:'33', coef:'1' },
+  { name: 'Cours 2', teacher:'Arnaud Castelltort', hours:'33', coef:'1' , intervenants:['Intervenant 1','Intervenant 2'] },
   { name: 'Cours 3', teacher:'Arnaud Castelltort', hours:'33', coef:'1', description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore ma' },
   { name: 'Cours 4'},
 ]
@@ -35,7 +35,7 @@ function Courses (props) {
           {
             courses.filter(course => course.name.toLowerCase().includes(search)).map((course, i) => // TODO
               <> <Collapse title={course.name} subtitle={course.teacher ? course.teacher : ''} key={i}>
-                  <CourseItem hours={course.hours} description={course.description} coef={course.coef} key={i} />
+                  <CourseItem hours={course.hours} description={course.description} coef={course.coef} intervenants={course.intervenants} key={i} />
                 </Collapse>
                 <br />
               </>
