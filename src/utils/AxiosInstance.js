30e406349a9a3174ac842f5c7d3419ace80130f8
Alia Chawaf <alia.chawaf@etu.umontpellier.fr>
2019-11-18 13:40:58 +0100

    FIX - Env var SERVER_URL

--- src/utils/AxiosInstance.js
+++ src/utils/AxiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import * as process from 'prop-types'
 
 const axiosInstance = axios.create({
-  baseURL: process.env.SERVER_URL,
+  baseURL: process.env.REACT_APP_SERVER_URL,
   timeout: 2000,
   responseType: 'json'
 })
