cf7852be0bda91847322c911c2b11a0ed07d0fcf
Samson Audrey <audrey.samson29@gmail.com>
2019-11-18 13:14:05 +0100

    fix bug axio

--- src/utils/AxiosInstance.js
+++ src/utils/AxiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import * as process from 'prop-types'
 
 const axiosInstance = axios.create({
-  baseURL: process.env.SERVER_URL,
+  //baseURL: process.env.SERVER_URL, BUG 
   timeout: 2000,
   responseType: 'json'
 })
