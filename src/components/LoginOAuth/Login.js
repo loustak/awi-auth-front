64e70f60db0d782a392129ee6eded683e4fce2ac
Alia Chawaf <alia.chawaf@etu.umontpellier.fr>
2019-11-19 04:00:41 +0100

    ADD - Warning message for missing param for login

--- src/components/LoginOAuth/Login.js
+++ src/components/LoginOAuth/Login.js
@@ -1,8 +1,22 @@
 import React from 'react'
 import '../../../src/App.css'
 import LoginForm from './LoginForm.js'
-import { Grid, Divider, Header, Segment, Message, Image } from 'semantic-ui-react'
+import { Grid, Divider, Header, Segment, Message, Image, Icon } from 'semantic-ui-react'
 import logo from '../../assets/logoPolytech.png'
+
+const requiredParams = ['client_id', 'state', 'redirect_uri']
+
+const MessageMissingParam = (props) => (
+  <Message negative>
+    <Message.Header>
+      <Icon name='warning sign' />
+      Warning
+    </Message.Header>
+    Your login will fail because the url does not contain
+    the parameter{props.params.length > 1 && 's'}
+    {' ' + props.params.join(', ')}.
+  </Message>
+)
 
 function Login () {
   const uriParams = {}
@@ -10,17 +24,22 @@ function Login () {
     uriParams[key] = value
   })
 
+  const missingParams = requiredParams.filter(p => !Object.keys(uriParams).includes(p))
+
   return (
 
     <Grid centered textAlign='center' style={{ height: '50vh' }} verticalAlign='middle'>
       <Grid.Column style={{ maxWidth: 350 }}>
-        <Divider hidden />
+        <Divider hidden/>
         <Message info>
-          <Image src={logo} size='mini' avatar />
+          <Image src={logo} size='mini' avatar/>
           Login with your Polytech account
         </Message>
-        <Segment>
+        <Divider hidden/>
 
+        {!missingParams.isEmpty && <MessageMissingParam params={missingParams } />}
+
+        <Segment>
           <Header as='h1' textAlign='center'>{uriParams.app_name ? uriParams.app_name : 'Login Page'}</Header>
           <Divider />
           <LoginForm redirect_uri={uriParams.redirect_uri} stateAuth={uriParams.state} client_id={uriParams.client_id} />
