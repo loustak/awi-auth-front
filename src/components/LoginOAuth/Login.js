e7c4349c795e26363c9d93e2a691636042badb2a
Alia Chawaf <alia.chawaf@etu.umontpellier.fr>
2019-11-19 03:17:31 +0100

    ADD - Info of login with polytech account

--- src/components/LoginOAuth/Login.js
+++ src/components/LoginOAuth/Login.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import '../../../src/App.css'
 import LoginForm from './LoginForm.js'
-import { Grid, Divider, Header } from 'semantic-ui-react'
+import { Grid, Divider, Header, Segment, Message, Image } from 'semantic-ui-react'
+import logo from '../../assets/logoPolytech.png'
 
 function Login () {
   const uriParams = {}
@@ -10,12 +11,20 @@ function Login () {
   })
 
   return (
+
     <Grid centered textAlign='center' style={{ height: '50vh' }} verticalAlign='middle'>
-      <Grid.Column style={{ maxWidth: 300 }}>
+      <Grid.Column style={{ maxWidth: 350 }}>
         <Divider hidden />
-        <Header as='h1' textAlign='center'>{uriParams.app_name ? uriParams.app_name : 'Login Page'}</Header>
-        <Divider />
-        <LoginForm redirect_uri={uriParams.redirect_uri} stateAuth={uriParams.state} client_id={uriParams.client_id} />
+        <Message info>
+          <Image src={logo} size='mini' avatar />
+          Login with your Polytech account
+        </Message>
+        <Segment>
+
+          <Header as='h1' textAlign='center'>{uriParams.app_name ? uriParams.app_name : 'Login Page'}</Header>
+          <Divider />
+          <LoginForm redirect_uri={uriParams.redirect_uri} stateAuth={uriParams.state} client_id={uriParams.client_id} />
+        </Segment>
       </Grid.Column>
     </Grid>
   )
