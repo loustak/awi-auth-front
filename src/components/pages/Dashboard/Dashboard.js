2bb4a9caf3da77d87323ada173e50b26e2503154
Aubin ABADIE <aubin.abadie@gmail.com>
2019-11-20 15:47:14 +0100

    Added doc and code cleaning

--- src/components/pages/Dashboard/Dashboard.js
+++ src/components/pages/Dashboard/Dashboard.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import Dashboard from '../../Dashboard/Dashboard'
-import * as test from '../../../services/apiFormatechService'
 
 class DashboardPage extends React.Component {
   render () {
     return (
       <>
-        {test.test()}
         <Dashboard />
       </>
     )
