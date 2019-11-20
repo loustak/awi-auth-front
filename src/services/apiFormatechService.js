7c7f3ee2843abcca6dd4f9d634aaf23e7ee81bde
Samson Audrey <audrey.samson29@gmail.com>
2019-11-20 10:48:49 +0100

    Update getTeacherSubjects

--- src/services/apiFormatechService.js
+++ src/services/apiFormatechService.js
@@ -9,7 +9,7 @@ export function getFormation (formation) {
 }
 
 export function test () {
-  getTeacherSubjects('IG', 3, 5)
+  getTeacherSubjects('Castelltort')
 }
 
 export function getStep (idStep) {
@@ -44,40 +44,81 @@ export function getSubject (idSubject) {
   })
 }
 
-export function getTeacherSubjects (formationName, stepNumber, teacher) {
+export function getTeacherSubjects (teacherName) {
+  const trainings = ['IG','MEA']
+  const steps = ['3','4','5']
+  const periods = ['5','6','7','8','9','10']
+  const res = {}
   return new Promise((resolve, reject) => {
-    getFormation(formationName)
-      .then(formation => {
-        const step = formation.steps.filter(s => s.title.includes(stepNumber))
-        getStep(step[0].id)
-          .then(step => {
-            delete step.id
-            delete step.title
-            formation.steps[0] = [formation.steps[0], step]
+    trainings.map(training => {
+      const resTraining = {}
+      getFormation(training)
+        .then(formation => {
+          steps.map(stepNumber => {
+            const step = formation.steps.filter(s => s.title.includes(stepNumber))[0]
+            return getStep(step.id)
+              .then(step => { // STEP DONE
+                delete step.id
+                delete step.title
+                Object.assign(formation.steps[stepNumber-3], step)
 
-            return formation
+                periods.map(periodNumber => { // PERIODE DONE
+                  const period = step.periods.filter(p => p.title.includes(periodNumber))[0]
+                  if (period) {
+                    return getPeriod(period.id).then( p => {
+                      Object.assign(step.periods.find(element => element.id === period.id), p)
+
+                      p.modules.map(module => {
+                        getModule(module.id)
+                          .then( m => {
+                            Object.assign(p.modules.find(element => element.id === module.id), m)
+
+                            module.subjects.map( subject => {
+                              getSubject(subject.id)
+                                .then( s => {
+                                  Object.assign( module.subjects.find(element => element.id === subject.id), s)
+                                })
+                            })
+                          })
+                      })
+                    })
+                  }
+                })
+              })
           })
-        console.log(formation)
-        return formation
-      })
-      .catch(err => reject(err))
+          console.log(formation)
+        })
+        .catch(err => reject(err))
+    })
+
   })
 }
 
 export function getPeriodSubjects (formationName, stepNumber, periodNumber) {
   return new Promise((resolve, reject) => {
-    getFormation(formationName)
-      .then(formation => {
-        const step = formation.steps.filter(s => s.title.includes(stepNumber))
-        getStep(step[0].id)
-          .then(step => {
-            delete step.id
-            delete step.title
-            formation.steps[0] = [formation.steps[0], step]
-            return formation
+    return new Promise((resolve, reject) => {
+      getFormation(formationName)
+        .then(formation => {
+          const step = formation.steps.filter(s => s.title.includes(stepNumber))[0]
+          return getStep(step.id)
+        })
+        .then(step => {
+          const period = step.periods.filter(p => p.title.includes(periodNumber))[0]
+          return getPeriod(period.id)
+        })
+        .then(period => {
+          period.modules.map(m => {
+            getModule(m.id)
+              .then(module => {
+                delete module.id
+                delete module.title
+                Object.assign(m, module)
+              })
           })
-        return formation
-      })
+
+          return period
+        })
       .catch(err => reject(err))
+    })
   })
 }
