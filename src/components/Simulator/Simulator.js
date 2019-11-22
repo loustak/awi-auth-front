0fbb46c843a45431511b5c6d0ef2fec15ad2174e
AlexandreKueny <alexandre.kueny@hotmail.com>
2019-11-22 14:53:31 +0100

    fixed focus

--- src/components/Simulator/Simulator.js
+++ src/components/Simulator/Simulator.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { connect } from 'react-redux'
 import styles from '../Dashboard/Dashboard.module.css'
 import Collapse from '../Collapse/Collapse'
@@ -16,26 +16,21 @@ function Simulator (props) {
 
   const periods = props.periods.periods.filter(period => period.modules && period.modules.length > 0)
 
-  const formik = useFormik({
-    initialValues: {
-      semester: ''
-    }
-  })
+  const [selectedSemester, setSelectedSemester] = useState('')
 
   const year = props.currentUser.user ? props.currentUser.user.section.substr(props.currentUser.user.section.length - 1) : null
   const training = props.currentUser.user ? props.currentUser.user.section.slice(0, -1).toUpperCase() : null
 
   const filteredSemesters = periods
-    .filter(semester => formik.values.semester !== '' ? semester.title === formik.values.semester : true)
-
+    .filter(semester => selectedSemester !== '' ? semester.title === selectedSemester : true)
 
   //-----------------------------FUNCTIONS-------------------------------------
 
   useEffect(() => {
     if (!props.periods.fetched && props.currentUser.fetched && !props.periods.fetching) {
-      setPeriodsSubjects( training, year)
+      setPeriodsSubjects(training, year)
     }
-  }, )
+  })
 
   //-----------------------------RETURN-------------------------------------
 
@@ -51,29 +46,34 @@ function Simulator (props) {
                     <Form.Label>Semestre</Form.Label>
                     <Form.Control
                       as='select'
-                      {...formik.getFieldProps('semester')}
                       className={styles.simulatorFormSemester}
+                      onChange={e => {
+                        setSelectedSemester(e.target.value)
+                        e.target.blur()
+                      }}
                     >
                       <option value=''>Choisissez un semestre</option>
                       {
-                        periods.map((period, i) => <option key={Math.random()} value={period.title}>{period.title}</option>)
+                        periods.map((period, i) => (
+                          <option key={Math.random()} value={period.title}>{period.title}</option>
+                        ))
                       }
                     </Form.Control>
                   </Form.Group>
                 </Form.Row>
               </Form>
               {
-                formik.values.semester !== ''
+                selectedSemester !== ''
                   ? <div className={styles.simulatorGlobalAverage}>
                     Moyenne : {markOperations.getGlobalAverage(filteredSemesters[0])}
-                </div>
+                  </div>
                   : null
               }
             </div>
             {
               filteredSemesters.length === 1
                 ? filteredSemesters[0].modules !== undefined
-                  ? filteredSemesters[0].modules.map((ue) => {
+                ? filteredSemesters[0].modules.map((ue) => {
                     return markOperations.getECTSFromUE(ue) > 0
                       ? <div key={Math.random()}>
                         <Collapse
@@ -98,19 +98,19 @@ function Simulator (props) {
                       </div>
                       : null
                   }
-                  )
-                  : <ErrorPage errorMessage='Aucune UE associée' />
+                )
+                : <ErrorPage errorMessage='Aucune UE associée' />
                 : null
             }
-            </>
+          </>
           : <div className={styles.loading}>
             <div className='spinner-border text-info' role="status">
-              <span className='sr-only'/>
+              <span className='sr-only' />
             </div>
             <div className={styles.loadingText}>
               <h4>Chargement en cours</h4>
             </div>
-            </div>
+          </div>
       }
     </>
   )
